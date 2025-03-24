375ec257a76e6f976afdbc6165fe5f20892f0f30
Haider Cheema <90347694+haiderali87@users.noreply.github.com>
2025-03-24 10:49:55 +0000

    Understand the variables concept is completely done

--- 001_Basics/01-variables.js
+++ 001_Basics/01-variables.js
@@ -1,1 +1,17 @@
-console.log("master");
+const user_id = 1;
+var user_name = "Haider Cheema";
+let user_age = 25;
+let user_state;
+user_city = "Sahiwal";
+
+// user_id = 2; //  you cannot reassign a value to a const variable once it's initialized. if you log it, it will through the error.
+user_name = "Hadi Cheema";
+user_age = 24;
+user_city = "Lahore";
+
+
+/*
+prefer not to use var
+because of issue with the block scope and functional scope
+*/
+console.table([user_id, user_name, user_age, user_city, user_state]);
