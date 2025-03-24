615206c1a004ee30e36d4c665f26dfc839bfa1c7
Haider Cheema <90347694+haiderali87@users.noreply.github.com>
2025-03-24 16:09:14 +0000

    Basic conversion operation studied

---
+++ 001_Basics/03-conversion-operation.js
@@ -0,0 +1,48 @@
+let score = "220abc";
+
+let updated_score = Number(score);
+
+// console.log(typeof updated_score);
+// console.log(updated_score);  
+
+/*
+
+Number Conversion
+*****************
+
+"220" => 220 
+"220abc" => NaN
+true => 1; false => 0
+
+Boolean Conversion
+******************
+
+0 => false;
+1 => true;
+"" => false
+"Haider" => true
+*/
+
+
+// ********************* Operations *********************
+let number = 10;
+let neg_number = -number;
+// console.log(number);
+// console.log(neg_number);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%5);
+
+let str1 = "Haider";
+let str2 = " Cheema";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+
+
+
