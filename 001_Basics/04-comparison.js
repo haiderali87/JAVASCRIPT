469d1cfff8e3f425cdbc6ed1ca401d930e676fe9
Haider Cheema <90347694+haiderali87@users.noreply.github.com>
2025-03-24 16:44:42 +0000

    Comparison Studied

---
+++ 001_Basics/04-comparison.js
@@ -0,0 +1,36 @@
+// There are two types of comparison in JS
+// 1. Loose comparison => (==) -> Checks values but allows type coercion.
+// 2. Strict comparison => (===) -> Checks values and types (recommended).
+
+// Loose (==) vs. Strict (===) Comparison
+// **************************************
+
+// console.log(5 == "5"); // true
+// console.log(5 === "5"); // false
+// console.log(0 == false); // ture
+// console.log(0 === false); // false
+// console.log(NaN == NaN); // false
+// console.log(NaN === NaN); // false
+
+
+// Inequality Operators
+// ********************
+
+// console.log(10 != "10"); // false because "10" convert to number so 10 is equal to 10 and we are checking inequality
+// console.log(10 !== "10"); // true because "10" remains string due to strict comparison (!==) so both are different and we get "true"
+
+
+
+// Greater & Less Than Operators
+// *****************************
+
+// console.log(100 > 50 ) // true
+// console.log(100 < 50); // false
+// console.log(100 > 100); // false
+// console.log(100 >=  100); // true
+// console.log(100 < 100); // false
+// console.log(100 <=  100); // true
+
+
+
+
